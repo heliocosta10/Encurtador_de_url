e8c3841279230108a32fae11450d599f48611136
Helio Costa <helioocosta18@gmail.com>
2022-05-16 22:22:17 -0300

    Create Constants.ts

---
+++ src/conf/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://<usuário>:<senha>@<banco de dados>.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+}
